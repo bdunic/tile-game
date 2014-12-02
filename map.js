8adc48840479c298d6ee9da7fd3092c15d866047
bdunic <bdunic@nebrwesleyan.edu>
2014-12-02 10:00:45 -0600

    Update map.js

--- map.js
+++ map.js
@@ -7,15 +7,15 @@ var row_count = 10;
 var column_count = 10;
 var terrain = [
     [[grass], [grass], [grass], [grass], [grass], [grass], [grass], [grass, treetall], [grass,treetall], [grass]],
-    [[grass], [grass], [grass, wood, wood], [grass], [wood], [grass], [grass, treeshort], [grass,treetall], [grass] [grass]],
-    [[grass], [grass, plain], [grass], [grass], [plain], [grass,treetall], [grass] [grass] [grass, treeshort], [grass]],
-    [[grass], [grass], [grass], [grass], [grass, plain, wood], [grass] [grass, treeshort], [grass],[grass] [grass, treetall]],
+    [[grass], [grass], [grass, wood, wood], [grass], [wood], [grass], [grass, treeshort], [grass,treetall], [grass], [grass]],
+    [[grass], [grass, plain], [grass], [grass], [plain], [grass,treetall], [grass], [grass], [grass, treeshort], [grass]],
+    [[grass], [grass], [grass], [grass], [grass, plain, wood], [grass] [grass, treeshort], [grass],[grass], [grass, treetall]],
     [[grass], [grass], [grass], [grass], [grass], [grass], [grass], [grass, treeshort], [grass,treeshort], [grass]],
-    [[grass], [grass], [grass, wood, wood], [grass], [wood], [grass], [grass, treetall], [grass,treetall], [grass] [grass]],
-    [[grass], [grass, plain], [grass], [grass], [plain], [grass,treetall], [grass] [grass] [grass, treetall], [grass]],
-    [[grass], [grass], [grass], [grass], [grass, plain, wood], [grass] [grass, treeshort], [grass],[grass] [grass, treeshort]],
+    [[grass], [grass], [grass, wood, wood], [grass], [wood], [grass], [grass, treetall], [grass,treetall], [grass], [grass]],
+    [[grass], [grass, plain], [grass], [grass], [plain], [grass,treetall], [grass], [grass] [grass, treetall], [grass]],
+    [[grass], [grass], [grass], [grass], [grass, plain, wood], [grass], [grass, treeshort], [grass],[grass], [grass, treeshort]],
     [[grass], [grass], [grass], [grass], [grass], [grass], [grass], [grass, treetall], [grass,treetall], [grass]],
-    [[grass], [grass], [grass, wood, wood], [grass], [wood], [grass], [grass, treetall], [grass,treetall], [grass] [grass]],
+    [[grass], [grass], [grass, wood, wood], [grass], [wood], [grass], [grass, treetall], [grass,treetall], [grass], [grass]],
 ];
 var occupants = [
     [undefined, undefined, undefined, undefined],
